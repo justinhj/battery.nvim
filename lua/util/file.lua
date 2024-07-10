4506e3ca6102aafaf142390608f0aebce77dcc5a
Eamon Burns (Laptop WSL) <me@eamonburns.com>
2024-07-09 17:01:02 -0700

    feat(util): use test command to check directory

--- lua/util/file.lua
+++ lua/util/file.lua
@@ -2,9 +2,13 @@ local M = {}
 
 local bit = require('bit')
 
-function M.is_readable_directory(file)
+function M.old_is_readable_directory(file)
   local s = vim.loop.fs_stat(file)
   return s ~= nil and s.type == 'directory' and bit.band(s.mode, 4) == 4
+end
+
+function M.is_readable_directory(file)
+  return os.execute("test -d "..file.." && test -r "..file) == 0
 end
 
 return M
