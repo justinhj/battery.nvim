6ae1a1dc0e5788311315ab5a267a64fa679f8d9a
artjsalina5 <artjsalina5@gmail.com>
2025-01-03 13:44:39 -0500

    attempt to fix wsl; added -Command

--- lua/battery/parsers/wsl.lua
+++ lua/battery/parsers/wsl.lua
@@ -6,23 +6,8 @@ local J = require('plenary.job')
 local L = require('plenary.log')
 local log = L.new({ plugin = 'battery' })
 
--- Convert status code from PowerShell to whether AC power is connected
-local status_code_to_ac_power = {
-  [1] = false, -- Battery Power
-  [2] = true, -- AC Power
-  [3] = true, -- Fully Charged
-  [4] = false, -- Low
-  [5] = false, -- Critical
-  [6] = true, -- Charging
-  [7] = true, -- Charging and High
-  [8] = true, -- Charging and Low
-  [9] = true, -- Charging and Critical
-  [10] = false, -- Undefined, we don't know so let's assume false
-  [11] = true, -- Partially Charged
-}
 local get_battery_info_powershell_command = {
-    'Get-CimInstance -ClassName Win32_Battery | \
- Select-Object -Property EstimatedChargeRemaining,BatteryStatus',
+  'Get-WmiObject -Class Win32_Battery | Select-Object -ExpandProperty EstimatedChargeRemaining',
 }
 
 ---Parse the response from the battery info job and update
@@ -33,7 +18,7 @@ local function parse_wsl_battery_info(result, battery_status)
   local battery_info = result[1]:match('%d+')
   if battery_info then
     battery_status.percent_charge_remaining = tonumber(battery_info)
-    battery_status.ac_power = status_code_to_ac_power[tonumber(battery_info)]
+    battery_status.ac_power = false
     battery_status.battery_count = 1
   else
     battery_status.percent_charge_remaining = 100
@@ -47,7 +32,8 @@ end
 function M.get_battery_info_job(battery_status)
   return J:new({
     command = '/mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe',
-    args = { '-Command', table.concat(get_battery_info_powershell_command, ' ') },    on_exit = function(j, return_value)
+    args = { '-Command', table.concat(get_battery_info_powershell_command, ' ') },
+    on_exit = function(j, return_value)
       if return_value == 0 then
         parse_wsl_battery_info(j:result(), battery_status)
         log.debug(vim.inspect(battery_status))
