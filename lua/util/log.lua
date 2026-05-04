4cd3d4679a064ccfb44015fee8848138abdd1cb4
Justin Heyes-Jones <justinhj@gmail.com>
2026-05-03 21:55:11 -0700

    fix backwards compat tests

--- lua/util/log.lua
+++ lua/util/log.lua
@@ -5,15 +5,17 @@ local M = {}
 local is_debug = os.getenv("BATTERY_DEBUG") == "true"
 local log_path = vim.fn.stdpath("state") .. "/battery.log"
 
+local uv = vim.uv or vim.loop
+
 local function log(msg, level)
   local time = os.date("%Y-%m-%d %H:%M:%S")
 
   local line = string.format("[%s][%s] %s\n", level, time, vim.inspect(msg))
 
-  local fd = vim.uv.fs_open(log_path, "a", 438)
+  local fd = uv.fs_open(log_path, "a", 438)
   if fd then
-    vim.uv.fs_write(fd, line)
-    vim.uv.fs_close(fd)
+    uv.fs_write(fd, line)
+    uv.fs_close(fd)
   end
 
   if level == "ERROR" then
--- test/spec/battery_plugin/battery_spec.lua
+++ test/spec/battery_plugin/battery_spec.lua
@@ -1,14 +1,31 @@
 local battery = require('battery.battery')
+local log = require('util.log')
 
 print("Running battery specs")
 
 local v = vim.version()
 local is_supported = v.major > 0 or v.minor >= 10
+
+-- Mock log.error to prevent it from showing notifications during tests
+local error_called = false
+local original_error = log.error
+log.error = function(msg)
+  error_called = true
+  print("Captured expected log.error: " .. tostring(msg))
+end
+
 local result = battery.check_version()
+
+-- Restore original error function
+log.error = original_error
 
 print(string.format("Neovim version: %d.%d.%d", v.major, v.minor, v.patch))
 print(string.format("Expected support: %s, Actual result: %s", tostring(is_supported), tostring(result)))
 
 assert(result == is_supported, "check_version result does not match expected support for this Neovim version")
+
+if not is_supported then
+  assert(error_called == true, "log.error should have been called for unsupported version")
+end
 
 print("All battery specs passed!")
